1f4e87d82e91f28ab9543d81141ca2cefd8b3409
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2023-08-05 11:24:42 +0530

    setup index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("connected");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log("server listening the port http://localhost/"+PORT);
+});
