1ce008066c5994d2473444807986483efecdb7b3
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2023-08-06 17:33:56 +0530

    Add register API, bodyParser, Nodemon

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get("/", (req, res) => {
-  res.send("connected");
+  res.json({ git: "good" });
 });
 
-const PORT = process.env.PORT || 3000;
+app.post("/api/register", urlencodedParser, (req, res) => {
+  const user = req.body;
+  console.log("request recieved");
+  // TODO: Check with all userNames in database
+  if (user.userName === "sid56rajawat") {
+    res.send("invalid username");
+  } else {
+    // store user in Database
+    // redirect to login page
+    res.send("valid username");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("server listening the port http://localhost/" + PORT);
 });
