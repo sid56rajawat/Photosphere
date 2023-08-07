8d49ff2e46a56f844177b727e9be7d676187cefb
sumeetbp <sumeetbpavitrakar@gmail.com>
2023-08-07 23:39:37 +0530

    Navigation Sidebar Creation and Responsive Styling

--- client/src/pages/Demo.js
+++ client/src/pages/Demo.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "../assets/css/Navbar.css"
 import Navbar from "../components/ui/Navbar";
 
 export default function Demo() {
   return (
-    <div className="page">
-      <Navbar />
-      <div className="home"></div>
+    <div className="demo">
+      <Navbar/>
     </div>
   );
-}
+};
