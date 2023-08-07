a0dedb2d8a2a620babc8a6ae85ce04cff93a6202
IMB003 <boradvatsal83@gmail.com>
2023-08-07 18:10:46 +0530

    Add PostPhoto Component

---
+++ client/src/components/ui/post/Postphoto/Photo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Photo() {
+  return(
+    <div className='photo'>
+        Photo
+    </div>
+  );
+}
+
+export default Photo;
---
+++ client/src/components/ui/post/Postphoto/Postphoto.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Photo from './Photo';
+
+export default function PostPhoto() {
+  return (
+    <div className="post-photo">
+      <Photo />
+    </div>
+  );
+}
+
